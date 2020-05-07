8d7de2933448b00eb4b499192d8d2eb473e50c35
sujithnath <sujithgnth@gmail.com>
2020-05-07 12:22:07 +0530

    custom html page

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'dist/',
+    publicPath: '',
   },
   mode: 'none',
   module: {
@@ -16,11 +20,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.js$/,
@@ -38,4 +42,22 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: [
+        '**/*',
+        path.join(process.cwd(), 'build/**/*'),
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Hello world',
+      meta: {
+        description: 'Some description',
+      },
+    }),
+  ],
 };
